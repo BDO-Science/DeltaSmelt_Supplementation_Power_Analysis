ac892dda942ab274fbe692fe2f8f54fe33cfada0
bmahardja <brianmahardja@gmail.com>
2023-09-13 14:20:37 -0700

    Fixed WY23 data wrangling code and added distance data. Note: 2 stations from EDSM removed due being out of bounds for the raster

--- R/R_WY2023_datawrangling.R
+++ R/R_WY2023_datawrangling.R
@@ -1,5 +1,4 @@
-# Purpose: Run power analysis for Delta Smelt Release Strategy study (Soft vs. Hard)
-#
+# Purpose: Prepare WY2023 Delta Smelt catch data for analysis
 # Author: Brian Mahardja
 # Date: 2023-09-06
 
@@ -21,7 +20,7 @@ library(boot)
 # Load Delta Smelt catch data from Catarina Pien for WY2023 so far
 smelt_data <-read.csv(file.path(data_root,"WY2023_deltasmelt_catch.csv")) %>% filter(Marked=="marked") %>%  
   mutate(Survey = str_replace_all(Survey, "CVP Salvage", "CVP")) %>%
-  mutate(Survey = str_replace_all(Survey, "SWP Salvage", "SWP")) 
+  mutate(Survey = str_replace_all(Survey, "SWP Salvage", "SWP")) %>% mutate(StationCode=ifelse(Survey %in% c("CVP","SWP"),Survey,StationCode))
 
 # Load hatchery fish release information from Catarina Pien
 release_info <- read.csv(file.path(data_root,"ReleaseInfo.csv")) %>% mutate(FirstDayRelease=as.Date(FirstDayRelease, "%m/%d/%Y"))
@@ -38,19 +37,19 @@ smelt_data_join <- smelt_data %>% left_join(release_info)  %>% mutate(Date=as.Da
 # Load WY 2023 EDSM xlsx data sent by Claudia on Sept 8 2023
 edsm_data <- read_excel(file.path(data_root,"22_23Phase1_Brian.xlsx")) %>% mutate(Count=ifelse(is.na(Count),0,Count)) %>%
   #Remove gear condition code 4
-  filter(GearConditionCode != 4) %>% group_by(StationCode,SampleDate) %>%
+  filter(GearConditionCode != 4) %>% mutate(TowNumber=ifelse(GearConditionCode==9,1,TowNumber)) %>% group_by(StationCode,TowNumber,SampleDate) %>%
   summarise(EDSM_Count=sum(Count)) %>% mutate(Survey="EDSM",StationCode=as.character(StationCode))
 
 # Load WY 2022-2023 SKT data sent by Vanessa Mora on Sept 6 2023
 skt_data <- read_excel(file.path(data_root,"SKT 2022-2023 Data.xlsx")) %>% rename(SKT_Catch = 'DS Catch') %>%
   #Use just WY 2023 data for year 2 dataset
   filter(Year==2023) %>%
-  select(SampleDate,SKT_Catch,StationCode,SurveyNumber) %>% mutate(Survey="SKT",StationCode=as.character(StationCode)) 
+  dplyr::select(SampleDate,SKT_Catch,StationCode,SurveyNumber) %>% mutate(Survey="SKT",StationCode=as.character(StationCode)) 
 
 # Load csv salvage data grabbed from DFW website
 # https://apps.wildlife.ca.gov/Salvage/Chart/AcrefeetSalvage?Adipose=All&SampMethod=Both&orgCode=26&orgDes=Delta%20Smelt&endDate=05%2F01%2F2023%2000%3A00%3A00&StartDate=11%2F01%2F2021%2000%3A00%3A00&ShowValue=False
 salvage_data <- read.csv(file.path(data_root,"SalvageExport.csv")) %>% rename(SampleDate=Sample.Date,Survey=Facility) %>%
-  mutate(SampleDate=as.Date(SampleDate, "%Y-%m-%d")) %>% select(-Species) %>%
+  mutate(SampleDate=as.Date(SampleDate, "%Y-%m-%d"),StationCode=Survey) %>% dplyr::select(-Species) %>%
   #There were NS data with 0 acre feet pumped for SWP, so remove because we assume no sampling was done
   filter(Salvage != "NS") %>% mutate(Salvage=as.numeric(Salvage)) %>% filter(SampleDate>="2022-11-01")
 
@@ -58,15 +57,15 @@ salvage_data <- read.csv(file.path(data_root,"SalvageExport.csv")) %>% rename(Sa
 allsurvey_data <- bind_rows(edsm_data,salvage_data,skt_data) %>% mutate(SampleDate = as.Date(SampleDate))
 
 
-temp_date_data <- release_info %>% select(Tag,FirstDayRelease,Total,Release.Method) %>%
+temp_date_data <- release_info %>% dplyr::select(Tag,FirstDayRelease,Total,Release.Method) %>%
   mutate(Release.Method = ifelse(Release.Method=="Trailer Release","Hard Release",Release.Method))
 
 # Summarize smelt_data_join data frame for instances where multiple fish were caught in the same sampling event
-temp_catch_data <- smelt_data_join %>% group_by(SampleDate,Survey,StationCode,Tag) %>% summarise(Count=sum(Count)) %>%
+temp_catch_data <- smelt_data_join %>% group_by(SampleDate,Survey,StationCode,Tag,TowNumber) %>% summarise(Count=sum(Count)) %>%
   # Convert "Count" column to just 1s and 0s
   mutate(Count=ifelse(Count>1,1,Count)) %>%
-  # Also convert CVP and SWP data to NA StationCode to match with salvage data
-  mutate(StationCode = ifelse(Survey %in% c("CVP","SWP"), NA, StationCode))
+  # Also convert CVP and SWP data StationCode to match with salvage data
+  mutate(StationCode = ifelse(Survey %in% c("CVP","SWP"), Survey, StationCode))
 
 list_data<-rep(list(allsurvey_data), 6)
 names(list_data)<- release_info$Tag
@@ -79,10 +78,19 @@ for (i in release_info$Tag) {
   
 }
 
-View(list_data[["ROP"]])
+sum(list_data[["LOA"]]$Count)
+sum(list_data[["LRA"]]$Count)
 
 # Combine datasets from all the release groups
 data_WY2023 <- data.frame(do.call(rbind, list_data)) %>% mutate(Survey=as.factor(Survey),Tag=as.factor(Tag),Release.Method=as.factor(Release.Method))
 
+distance_data <- read.csv(file.path(output_root,"WY2023_distance_from_release_covariate.csv")) %>% select(StationCode,Tag,dist_km)
+
+# Add in distance from release site as data
+data_WY2023 <- left_join(data_WY2023,distance_data,multiple="all")
+
+# Remove infinite distance values from a couple of stations: 23-26-SM01, 23-39-SD07
+data_WY2023 <- data_WY2023 %>% filter(dist_km!="Inf")
+
 # Export dataset out
-write.csv(data_WY2023,file=file.path(output_root,"WY2023_catch_data.csv"))
+write.csv(data_WY2023,file=file.path(output_root,"WY2023_catch_data.csv"),row.names = F)
---
+++ R/R_WY2023_distance_from_release_site.R
@@ -0,0 +1,103 @@
+# Purpose: Prepare swim distance data for analysis
+# Author: Brian Mahardja
+# Date: 2023-09-12
+
+# Set working directory
+root <- "C:/Users/bmahardja/Documents/GitHub/DeltaSmelt_Supplementation_Power_Analysis"
+setwd(root)
+
+data_root<-file.path(root,"data")
+code_root <- file.path(root,"R")
+output_root <- file.path(root,"output")
+
+# Load Package Libraries
+library(gdistance)
+library(raster)
+library(sf)
+library(sp)
+library(rgdal)
+library(tidyverse)
+library(readxl)
+library(ggspatial)
+library(lubridate)
+library(viridis)
+library(readr)
+library(rgeos)
+library(deltafish)
+
+# Bay Delta Raster TIP with cross channel closure
+#TIF raster file was made in Arc GIS Pro from the bay delta shapefile with a 50 meter buffer. 
+#TIF stored the associated CRS information. PNG did not.  
+#Tile size was set at 50m x 50m to try and keep the file size and processing time manageable. 
+#Conversion of the shapefile to a raster in R did not work properly. It merged across sloughs in the Delta resulting in one giant polygon of water. 
+raster_baydelta<-raster(file.path(data_root,"baydelta.tif"))
+raster_baydelta #Check out resolution information and CRS = 26910 (NAD 1983 UTM Zone 10)
+raster_baydelta[is.na(raster_baydelta)] <- 0  # code land as 0, meaning no passage is possible
+plot(raster_baydelta) #Check Raster file to make sure water ways look okay
+
+# Read the pre-processed raster file since the conversion takes several minutes
+raster_baydelta_tr <- readRDS(file.path(data_root,"raster_baydelta_tr.rds"))
+
+# Load WY 2023 EDSM xlsx data sent by Claudia on Sept 8 2023 and grab station coordinates
+edsm_stations <- read_excel(file.path(data_root,"22_23Phase1_Brian.xlsx")) %>% mutate(Count=ifelse(is.na(Count),0,Count)) %>%
+  #Remove gear condition code 4
+  filter(GearConditionCode != 4) %>% mutate(TowNumber=ifelse(GearConditionCode==9,1,TowNumber)) %>%
+  mutate(Survey="EDSM",StationCode=as.character(StationCode)) %>%
+  #To reduce calculation time, we're going to combine lat and long to each station
+  group_by(StationCode,Survey) %>% summarise(Latitude=median(LatitudeStart),Longitude=median(LongitudeStart))
+
+# Compile SKT tation Coordinates
+skt_stations <- read_csv(file.path(data_root,"SKTstations.csv")) %>%
+  dplyr::select(-1, -Description) %>%
+  mutate(Longitude = Longitude * -1) %>% rename(StationCode=Station) %>% mutate(StationCode=as.character(StationCode))
+  
+
+# Compile coordinates for Tracy and Skinner facilities
+# TFCF: 37.815176 -121.560709 (WGS84)
+# Skinner: 37.82968216253291, -121.55652895353651
+
+salvage_stations <- data.frame(Survey = c("CVP", "SWP"),
+                          StationCode = c("CVP","SWP"),
+                          Latitude = c(37.815176,37.82968216253291),
+                          Longitude = c(-121.560709, -121.55652895353651))
+# Combine datasets into 1 and convert to SF
+station_all <- bind_rows(edsm_stations, skt_stations, salvage_stations)
+
+test<-station_all %>% filter(StationCode=="23-31-SSC02")
+
+station_all_sf <- station_all %>% filter(Longitude > -500) %>%
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
+  st_transform(crs = st_crs(raster_baydelta))
+plot(station_all_sf)
+
+# Add release sites
+releases <- read_excel(file.path(data_root,"release_locations_dates_2023.xlsx")) %>% rename(Tag=Mark)
+
+releases$Tag <- str_replace(releases$Tag ,'AdClipped','ad')
+
+releases_sf <- releases %>%
+  mutate(Latitude = as.numeric(Latitude),
+         Longitude = as.numeric(Longitude)) %>%
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
+  st_transform(crs = st_crs(raster_baydelta))
+
+
+# Calculate distances with least cost analysis
+dist_2023 <- costDistance(raster_baydelta_tr,
+                          fromCoords = as(as_Spatial(station_all_sf), "SpatialPoints"),
+                          toCoords = as(as_Spatial(releases_sf), "SpatialPoints"))
+rownames(dist_2023)<-as.vector(station_all_sf$StationCode)
+colnames(dist_2023)<-as.vector(releases_sf$Tag)
+
+# Turn back to dataframe
+Least_Cost_data_2023 <-data.frame(dist_meters = as.vector(as.matrix(dist_2023)),
+                                  comparison = as.vector(outer(Y=colnames(as.matrix(dist_2023)) ,
+                                                               X=rownames(as.matrix(dist_2023)) ,
+                                                               paste, sep="xxx"))) %>%
+  mutate(StationCode=sub("xxx.*","", comparison),Tag=sub(".*xxx","", comparison),dist_km=dist_meters/1000) %>%
+  dplyr::select(StationCode,Tag,dist_km)
+
+# Export dataset out
+write.csv(Least_Cost_data_2023,file=file.path(output_root,"WY2023_distance_from_release_covariate.csv"),row.names = F)
+
+
